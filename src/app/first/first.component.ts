a616712da545672c34676d03d2c3f5a644ed75ed
Nidhal <nidhal@N2SY.local>
2023-10-25 12:16:30 +0100

    bindings

---
+++ src/app/first/first.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first',
+  templateUrl: './first.component.html',
+  styleUrls: ['./first.component.css'],
+})
+export class FirstComponent {
+  prenom = 'lobna';
+  color = 'pink';
+  hd = true;
+
+  afficherAlert() {
+    alert('Le bouton a été cliqué !');
+  }
+}
