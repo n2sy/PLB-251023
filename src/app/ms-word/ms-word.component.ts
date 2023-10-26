116b1069b0021e2bb48ee7165009caacc50fe55b
Nidhal <nidhal@N2SY.local>
2023-10-26 10:05:20 +0100

    final step ngstyle exercice

--- src/app/ms-word/ms-word.component.ts
+++ src/app/ms-word/ms-word.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-ms-word',
   templateUrl: './ms-word.component.html',
-  styleUrls: ['./ms-word.component.css']
+  styleUrls: ['./ms-word.component.css'],
 })
 export class MsWordComponent {
+  txtBg;
+  txtCl;
+  txtSize;
+  txtFont;
 
+  changeSize(newSize) {
+    console.log(newSize);
+    let unite = prompt("Saisissez l'unité");
+    this.txtSize = newSize + unite;
+  }
 }
